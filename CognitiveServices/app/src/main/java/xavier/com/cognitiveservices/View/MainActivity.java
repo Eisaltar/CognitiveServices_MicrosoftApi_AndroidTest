f1aefef57664612a5102334089a965a1b483b841
Dutrieue Xavier <xav314@gmail.com>
2016-11-30 23:07:39 +0100

    first publish
    error internet access

---
+++ CognitiveServices/app/src/main/java/xavier/com/cognitiveservices/View/MainActivity.java
@@ -0,0 +1,202 @@
+package xavier.com.cognitiveservices.View;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import xavier.com.cognitiveservices.R;
+import com.microsoft.projectoxford.face.*;
+import com.microsoft.projectoxford.face.contract.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static int REQUEST_IMAGE_CAPTURE = 1;
+
+    private ImageView pictureDisplay;
+    private Button analyzePictureButton;
+    private TextView displayAnalyze;
+    private Bitmap bitmap;
+    private String textToDisplay;
+    private static FaceServiceClient faceServiceClient = new FaceServiceRestClient("2563970663064fb6b943bc4e610f7127");
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        init();
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(this);
+    }
+
+    private void init() {
+        pictureDisplay = (ImageView) this.findViewById(R.id.pictureDisplay);
+        displayAnalyze = (TextView) this.findViewById(R.id.descriptionDisplay);
+        analyzePictureButton = (Button) this.findViewById(R.id.buttonAnalyze);
+        analyzePictureButton.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void launchAnalyze( ) {
+        String result= launchFaceAnalyze ();
+
+
+    }
+
+    private String launchFaceAnalyze() {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+        AsyncTask<InputStream, String, Face[]> detectTask =
+                new AsyncTask<InputStream, String, Face[]>(){
+                    @Override
+                    protected Face[] doInBackground(InputStream... params) {
+                        try {
+                            publishProgress("Detecting...");
+                            Face[] result = faceServiceClient.detect(params[0],true,false,null);
+                            if (result == null)
+                            {
+                                publishProgress("Detection Finished. Nothing detected");
+                                return null;
+                            }
+                            publishProgress(
+                                    String.format("Detection Finished. %d face(s) detected",
+                                            result.length));
+                            return result;
+                        } catch (Exception e) {
+                            publishProgress("Detection failed");
+                            e.printStackTrace();
+                            return null;
+                        }
+                    }
+                    @Override
+                    protected void onPreExecute() {
+                        Toast.makeText(MainActivity.this, "début de l'analyse", Toast.LENGTH_SHORT).show();
+                    }
+                    @Override
+                    protected void onProgressUpdate(String... progress) {
+                        Toast.makeText(MainActivity.this, "progress", Toast.LENGTH_SHORT).show();
+                    }
+                    @Override
+                    protected void onPostExecute(Face[] result) {
+                        if (result == null){
+                            displayAnalyze.setText("error");
+                        }else{
+                            displayAnalyze.setText(result.toString());
+                            bitmap = drawFaceREctangleOnBitmap (bitmap, result);
+                            pictureDisplay.setImageBitmap(bitmap);
+
+                        }
+
+                    }
+
+                };
+        detectTask.execute(inputStream);
+        return null;
+    }
+
+    private Bitmap drawFaceREctangleOnBitmap(Bitmap bitmap, Face[] result) {
+        Bitmap bitmapDraw = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+        Canvas canvas = new Canvas(bitmapDraw);
+        Paint paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(Color.RED);
+        int stokeWidth = 2;
+        paint.setStrokeWidth(stokeWidth);
+        if (result != null) {
+            for (Face face : result) {
+                FaceRectangle faceRectangle = face.faceRectangle;
+                canvas.drawRect(
+                        faceRectangle.left,
+                        faceRectangle.top,
+                        faceRectangle.left + faceRectangle.width,
+                        faceRectangle.top + faceRectangle.height,
+                        paint);
+                if (face.faceAttributes != null) {
+                    textToDisplay = new String();
+                    textToDisplay = "this is a "+ face.faceAttributes.gender;
+                    textToDisplay += " this person is " + face.faceAttributes.age;
+                    textToDisplay += " facial hair : "+ face.faceAttributes.facialHair;
+                    textToDisplay += " smile : "+face.faceAttributes.smile;
+                    textToDisplay += " head pose : " + face.faceAttributes.headPose;
+
+                    displayAnalyze.setText( textToDisplay);
+                }
+
+            }
+        }
+        return bitmap;
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch(id){
+            case R.id.buttonAnalyze:
+                launchAnalyze();
+                break;
+            case R.id.fab:
+                launchTakingPicture();
+                break;
+        }
+
+    }
+
+    private void launchTakingPicture() {
+        Intent takePictureIntent = new Intent (MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult (int requestCode, int resultCode, Intent data){
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK ){
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            bitmap = imageBitmap;
+            pictureDisplay.setImageBitmap(bitmap);
+        }
+    }
+}
